61ca687ac1060e36a26f0a2a31a529c5d02cb80b
Nicolas Leigh <linze201@gmail.com>
2024-11-10 21:53:22 +0800

    creating writeJSON helper func

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -29,32 +28,50 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 */
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {  
+/*
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
   // Create a map which holds the information that we want to send in the response.
-  data := map[string]string{    
-    "status":      "available",   
-    "environment": app.config.env,     
-    "version":     version,   
+  data := map[string]string{
+    "status":      "available",
+    "environment": app.config.env,
+    "version":     version,
   }
-  
-  // Pass the map to the json.Marshal() function. This returns a []byte slice  
-  // containing the encoded JSON. If there was an error, we log it and send the client 
+
+  // Pass the map to the json.Marshal() function. This returns a []byte slice
+  // containing the encoded JSON. If there was an error, we log it and send the client
   // a generic error message.
-  js, err := json.Marshal(data)   
-  if err != nil { 
-    app.logger.Error(err.Error())      
-    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)    
-    return  
-  } 
-  
-  // Append a newline to the JSON. This is just a small nicety to make it easier to 
+  js, err := json.Marshal(data)
+  if err != nil {
+    app.logger.Error(err.Error())
+    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+    return
+  }
+
+  // Append a newline to the JSON. This is just a small nicety to make it easier to
   // view in terminal applications.
-  js = append(js, '\n')    
-  
-  // At this point we know that encoding the data worked without any problems, so we  
+  js = append(js, '\n')
+
+  // At this point we know that encoding the data worked without any problems, so we
   // can safely set any necessary HTTP headers for a successful response.
-  w.Header().Set("Content-Type", "application/json")   
-  
+  w.Header().Set("Content-Type", "application/json")
+
   // Use w.Write() to send the []byte slice containing the JSON as the response body.
-  w.Write(js) 
+  w.Write(js)
+}
+
+*/
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {   
+  data := map[string]string{     
+    "status":      "available",    
+    "environment": app.config.env,    
+    "version":     version,    
+  }   
+  
+  err := app.writeJSON(w, http.StatusOK, data, nil)    
+  if err != nil {       
+    app.logger.Error(err.Error())    
+    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)  
+  }
 }
